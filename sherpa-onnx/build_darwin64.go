2c8951776115b11d63c22d5a54e251185e7ac0d4
Fangjun Kuang <csukuangfj@gmail.com>
2023-07-17 18:03:14 +0800

    fix a typo

--- sherpa-onnx/build_darwin64.go
+++ sherpa-onnx/build_darwin64.go
@@ -2,5 +2,5 @@
 
 package sherpa_onnx
 
-// #cgo LDFLAGS: -L ${SRCDIR}/lib/x86_64-apple-darwin -lsherpa-onnx-c-api -lsherpa-onnx-core -lkaldi-native-fbank-core -lonnxruntime -Wl,-rpath,${SRCDIR}/lib/x86_64-apple-darwin
+// #cgo LDFLAGS: -L ${SRCDIR}/../lib/x86_64-apple-darwin -lsherpa-onnx-c-api -lsherpa-onnx-core -lkaldi-native-fbank-core -lonnxruntime -Wl,-rpath,${SRCDIR}/../lib/x86_64-apple-darwin
 import "C"
